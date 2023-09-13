1dc5531794c2b452d9ef220bc3c7ffade9b5940c
junym <jullan.nyman@hotmail.com>
2023-09-13 09:55:25 +0200

    testat lite till labb2

--- test-labb2.py
+++ test-labb2.py
@@ -1,46 +1,45 @@
-'''
-def beräkna_an(startvärde, differens, antal_tal):
-    return(startvärde + differens * (antal_tal - 1))
+"""Labb 2: 
+Program som beräknar den aritmetiska och geometriska summan beroende på valda inputs."""
 
-startvärde = 1
-differens = 2
-antal_tal = 4
+def beräkna_ari_summa(startvärde, differens, antal_tal):
+    """Beräknar den aritmetiska summan beroende på startvärde, differens, antal tal och det n:te talet."""
+    an = startvärde + differens * (antal_tal - 1)
+    aritmetisk_summa = antal_tal * (startvärde + an) / 2 
+    return aritmetisk_summa
 
-print(beräkna_an(startvärde, differens, antal_tal))
+def beräkna_geo_summa(startvärde, kvoten, antal_tal):
+    """Beräknar den geometriska summan beroende på startvärde, kvoten och antal tal."""
+    geometrisk_summa = startvärde*(((kvoten**antal_tal)-1) / (kvoten - 1))
+    return geometrisk_summa
 
-def beräkna_aritmetisk_summa(startvärde, differens, antal_tal):
-    return(antal_tal * (startvärde + beräkna_an(startvärde, differens, antal_tal)) / 2)
 
-print(beräkna_aritmetisk_summa(startvärde, differens, antal_tal))
+def huvudprogram():
+    """Tar emot inputs från användaren om vilket startvärde, differens, kvot och antal tal, 
+    och printar sen ut den aritmetiska respektive geometriska summan samt vilken av summorna som är störst."""
+    try:
+        startvärde = float(input("Vad väljer du för startvärde till den aritmetiska talföljden? "))
+        differens = float(input("Vad är differensen? "))
+        antal_tal = float(input("Hur många tal i talföljden? "))
+        print()
+        print(f"Då blir den aritmetiska summan: {beräkna_ari_summa(startvärde, differens, antal_tal)}")
+    except ValueError:
+        print("Oj det blev fel, det måste vara ett flyttal, starta om programmet och kör igen")    
+    print()
 
-startvärde = 1
-kvoten = 2
-antal_tal = 4
-def beräkna_geometrisk_summa(startvärde, kvoten, antal_tal):
-    return(startvärde*(((kvoten**antal_tal)-1) / (kvoten - 1)))
+    try:
+        startvärde = float(input("Vad väljer du för startnummer till den geometriska talföljden? "))
+        kvoten = float(input("Vad väljer du för kvot? "))
+        antal_tal = float(input("Hur många tal vill du ha i din följd? "))
+        print()
+        print(f"Då blir den geometriska summan: {beräkna_geo_summa(startvärde, kvoten, antal_tal)}")
+    except ValueError:
+        print("Oj det blev fel, det måste vara ett flyttal, starta om programmet och kör igen")
+    print()
 
-print(beräkna_geometrisk_summa(startvärde, kvoten, antal_tal)) '''
+    if beräkna_ari_summa(startvärde, differens, antal_tal) > beräkna_geo_summa(startvärde, kvoten, antal_tal):
+        print("Den aritmetiska summan är större!")
+    else:
+        print("Den geometriska summan är större!") 
+    print()
 
-startvärde = int(input("Vad väljer du för startnummer? "))
-differens = int(input("Vad väljer du för differens? "))
-antal_tal = int(input("Hur många tal vill du ha i din följd? "))
-
-def beräkna_an(startvärde, differens, antal_tal):
-    """Funktionen beräknar an"""
-    return(startvärde + differens * (antal_tal - 1))
-
-def beräkna_aritmetisk_summa(startvärde, differens, antal_tal):
-    """Funktionen beräknar den aritmetiska summan beroende på valda inputs"""
-    return(antal_tal * (startvärde + beräkna_an(startvärde, differens, antal_tal)) / 2)
-
-print (f"Den aritmetiska summan blir: {beräkna_aritmetisk_summa(startvärde, differens, antal_tal)}")
-
-startvärde = int(input("Vad väljer du för startnummer? "))
-kvoten = int(input("Vad väljer du för kvot? "))
-antal_tal = int(input("Hur många tal vill du ha i din följd? "))
-
-def beräkna_geometrisk_summa(startvärde, kvoten, antal_tal):
-    """Funktionen beräknar den geometriska summan beroende på valda inputs"""
-    return(startvärde*(((kvoten**antal_tal)-1) / (kvoten - 1)))
-
-print(f"Den geometriska summan blir: {beräkna_geometrisk_summa(startvärde, kvoten, antal_tal)}")
+huvudprogram()
